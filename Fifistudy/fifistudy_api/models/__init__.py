7ee8fe6e51b3ce9170ff9c10a62f2153797cc344
Sinhnt <nguyentrisinh0810@gmail.com>
2017-10-21 12:04:11 +0700

    Commit models for fifistudy (part 1)

---
+++ Fifistudy/fifistudy_api/models/__init__.py
@@ -0,0 +1,7 @@
+from .blog import Blog
+from .category import Category
+from .fifi_user import FifiUser
+from .film import Film
+from .note import Note
+from .promote import Promote
+from .type import Type
