781468326fc584606d42832c7c6f9c912b6177b9
Umino <umino.sharp@gmail.com>
2017-10-23 23:38:30 +0700

    Add redux library

--- Fifistudy_ReactNative/FiFistudy/src/App.js
+++ Fifistudy_ReactNative/FiFistudy/src/App.js
@@ -2,14 +2,32 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
+  Image,
+  Button,
   View
 } from 'react-native';
+import { DrawerNavigator } from 'react-navigation';
+import HomePage from './screens/HomeScreen';
+
 
 export default class App extends Component {
+  static navigationOptions = {
+    drawerLabel: 'ABC',
+    drawerIcon: ({tintColor}) => (
+      <Image source={require('./ic_menu.png')}
+        style={{
+          width: 24,
+          height: 24,
+          tintColor: tintColor
+        }}/>
+    ),
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-      </View>
+      // <Button onPress={() => this.props.navigation.navigate('HomePage')}
+      // title='Go to notifications'/>
+      <View/>
     );
   }
 }
