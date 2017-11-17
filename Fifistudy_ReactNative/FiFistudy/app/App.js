037add7b349b0112cd91ac3044011f0dac5e5800
nhan <thiennhank9@gmail.com>
2017-11-17 09:20:23 +0700

    Set-up redux

--- Fifistudy_ReactNative/FiFistudy/app/App.js
+++ Fifistudy_ReactNative/FiFistudy/app/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import DrawerApp from './Navigators/DrawerApp.js';
+import store from './Redux/Store.js';
+import { Provider } from 'react-redux';
 
 class App extends Component {
     render() {
         return (
-            <DrawerApp />
+            <Provider store={store}>
+                <DrawerApp />
+            </Provider>
         )
     }
 }
