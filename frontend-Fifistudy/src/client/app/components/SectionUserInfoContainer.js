4f27e1baef715fa651977fec762ffd666eaf5c2d
Unknown <liemnt.uit@gmail.com>
2017-11-25 00:33:50 +0700

    Edit Info

---
+++ frontend-Fifistudy/src/client/app/components/SectionUserInfoContainer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Vocabulary from './Vocabulary'
+import {connect} from 'react-redux';
+import FlipMove from 'react-flip-move';
+import SectionUserInfo from './SectionUserInfo';
+import Input1 from './Input1';
+import _ from 'lodash';
+
+class SectionUserInfoContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        if (_.has(this.props.userInfo,"data")){
+            if (this.props.userInfo.data.errors ===null){
+                if (typeof this.props.userInfo.data.data =="object"){
+                    return <SectionUserInfo data={this.props.userInfo.data.data}/>
+                }
+            }
+        }
+        return <div>
+            Trang lỗi
+        </div>
+    }
+}
+
+const mapStateToProps = state =>    {
+    return {
+        userInfo:state.app.userInfo
+    }
+}
+
+export default connect(mapStateToProps)(SectionUserInfoContainer)
